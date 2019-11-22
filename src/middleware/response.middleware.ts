c8534c48caf4c3c84d8bfd8bfba078565b0f371f
Dede Kurniawan <dekur98@outlook.com>
2019-11-22 13:19:39 +0700

    litle fixing

--- src/middleware/response.middleware.ts
+++ src/middleware/response.middleware.ts
@@ -3,5 +3,5 @@ import { IResponse } from '../interfaces';
 
 export default (structured: any, request: express.Request, response: express.Response, next: express.NextFunction): void => {
 	global.logger.info(structured);
-	response.status((structured.code) ? structured.code : 500).json(structured);
+	response.status((structured.code) ? Number.isInteger(structured.code) ? structured.code :500 : 500).json(structured);
 };
