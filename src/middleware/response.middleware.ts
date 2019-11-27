b1d139d9813db77a22e585e349d2d3ef1c32c235
Dede Kurniawan <dekur98@outlook.com>
2019-11-27 13:24:15 +0700

    change pino logger to winston

--- src/middleware/response.middleware.ts
+++ src/middleware/response.middleware.ts
@@ -2,5 +2,6 @@ import express from 'express';
 
 export default (structured: any, request: express.Request, response: express.Response, next: express.NextFunction): void => {
 	global.logger.info(structured);
-	response.status((structured.code) ? structured.code : 500).json(structured);
+	// eslint-disable-next-line no-nested-ternary
+	response.status((structured.code) ? Number.isInteger(structured.code) ? structured.code : 500 : 500).json(structured);
 };
