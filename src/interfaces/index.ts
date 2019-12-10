cc546cd6fe42d10bb27f2918389da3e3a6567e92
Dede Kurniawan <dekur98@outlook.com>
2019-12-10 22:38:22 +0700

    adding firebase

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
+import firebaseAdmin from 'firebase-admin';
 import User from '../modules/user/user.model';
 
 export interface Error {
@@ -27,4 +28,11 @@ export interface IServerOptions {
     port: number;
     appName?: string;
     appVersion?: string;
+}
+
+
+export interface IFirebaseAdminSendNotification {
+    firebaseToken: string;
+    payload: firebaseAdmin.messaging.MessagingPayload;
+    options?: firebaseAdmin.messaging.MessagingOptions;
 }
